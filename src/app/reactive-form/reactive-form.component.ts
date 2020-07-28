0516db3bd11e019364cf6b428bb95c0f383e4a9b
Menna <menna.elnokally@gmail.com>
2020-07-29 01:20:57 +0200

    ✨ NEW FEATURE: Constructing form with form builder

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-reactive-form',
-  templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.css']
+  selector: "app-reactive-form",
+  templateUrl: "./reactive-form.component.html",
+  styleUrls: ["./reactive-form.component.css"],
 })
 export class ReactiveFormComponent implements OnInit {
-
-  constructor() { }
+  infoForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.infoForm = this.formBuilder.group({
+      firstName: ["", [Validators.required, Validators.minLength(3)]],
+      lastName: ["", [Validators.required, Validators.minLength(3)]],
+    });
   }
-
 }
