9fca8b72d12fddb5bd6b4328fec35091e8ca41b7
Menna <menna.elnokally@gmail.com>
2020-07-29 01:00:11 +0200

    ✨ NEW FEATURE: Reactive forms component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormComponent
   ],
   imports: [
     BrowserModule,
