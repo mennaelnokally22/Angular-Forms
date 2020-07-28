c990c55d6bed3ee513cb74155e13733ab8a3d4f2
Menna <menna.elnokally@gmail.com>
2020-07-29 01:51:26 +0200

    💫 UPDATE: Added new form group

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from "@angular/forms";
+
+import { debounceTime } from "rxjs/operators";
 
 @Component({
   selector: "app-reactive-form",
@@ -14,6 +21,12 @@ export class ReactiveFormComponent implements OnInit {
     this.infoForm = this.formBuilder.group({
       firstName: ["", [Validators.required, Validators.minLength(3)]],
       lastName: ["", [Validators.required, Validators.minLength(3)]],
+      emailGroup: this.formBuilder.group({
+        email: ["", [Validators.required, Validators.email]],
+        confirmEmail: ["", [Validators.required]],
+      }),
     });
+
+    this.infoForm.get("firstName").valueChanges.pipe(debounceTime(1000));
   }
 }
