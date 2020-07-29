e3cb1b563f0c58205d37489474b1c354b6967d8d
Menna <menna.elnokally@gmail.com>
2020-07-29 03:10:55 +0200

    ✨ NEW FEATURE: Adding a password form group

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -15,6 +15,7 @@ function controlsMatcher(controlGroupName: string): ValidatorFn {
     const firstControl = control.get(controlGroupName);
     const secControl = control.get(`confirm${controlGroupName}`);
 
+    console.log(firstControl.value, secControl.value);
     return firstControl.value === secControl.value ? null : { missMatch: true };
   };
 }
@@ -32,10 +33,20 @@ export class ReactiveFormComponent implements OnInit {
     this.infoForm = this.formBuilder.group({
       firstName: ["", [Validators.required, Validators.minLength(3)]],
       lastName: ["", [Validators.required, Validators.minLength(3)]],
-      emailGroup: this.formBuilder.group({
-        email: ["", [Validators.required, Validators.email]],
-        confirmEmail: ["", [Validators.required]],
-      }),
+      emailGroup: this.formBuilder.group(
+        {
+          email: ["", [Validators.required, Validators.email]],
+          confirmemail: ["", [Validators.required]],
+        },
+        { validators: controlsMatcher("email") }
+      ),
+      passwordGroup: this.formBuilder.group(
+        {
+          password: ["", [Validators.required, Validators.minLength(6)]],
+          confirmpassword: ["", [Validators.required]],
+        },
+        { validators: controlsMatcher("password") }
+      ),
     });
 
     this.infoForm.get("firstName").valueChanges.pipe(debounceTime(1000));
