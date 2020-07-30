d576232888a198fab9c32a1a451d1e44a0aa44f5
Menna <menna.elnokally@gmail.com>
2020-07-30 13:08:39 +0200

    ✨ NEW FEATURE: Template driven component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ReactiveFormComponent } from "./reactive-form/reactive-form.component";
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 
 @NgModule({
-  declarations: [AppComponent, ReactiveFormComponent],
+  declarations: [AppComponent, ReactiveFormComponent, TemplateDrivenFormComponent],
   imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
