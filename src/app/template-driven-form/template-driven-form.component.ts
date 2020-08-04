92f91ba674b076954621b3a0537cd665dfe90558
Menna <menna.elnokally@gmail.com>
2020-08-04 02:24:25 +0200

    ✨ NEW FEATURE: Validating and submiting the form

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-template-driven-form',
-  templateUrl: './template-driven-form.component.html',
-  styleUrls: ['./template-driven-form.component.css']
+  selector: "app-template-driven-form",
+  templateUrl: "./template-driven-form.component.html",
+  styleUrls: ["./template-driven-form.component.css"],
 })
 export class TemplateDrivenFormComponent implements OnInit {
+  info: Object = { firstName: "", email: "", password: "" };
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onSubmit(infoForm: NgForm) {
+    console.log(infoForm.value);
   }
-
 }
